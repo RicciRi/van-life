5788561012a7ba18cb1fe0269c37194f1d70dbfe
Ricci.rudenko <richardrudenko555@gmail.com>
2023-12-22 19:31:52 +0100

    Update Vans.jsx

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 
 export default function Vans() {
@@ -12,12 +13,14 @@ export default function Vans() {
 
     const vanElement = vans.map((van) => (
         <section key={van.id} className="section-items">
-            <img className="car-image" src={van.imageUrl} alt="Car" />
-            <div className="van-info">
-                <h3>{van.name}</h3>
-                <p>${van.price}<span>/day</span></p>
-            </div>
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            <Link to={`/vans/${van.id}`}>
+                <img className="car-image" src={van.imageUrl} alt="Car" />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
         </section>
     ))
 
