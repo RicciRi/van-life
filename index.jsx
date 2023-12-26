94664533497d237536ba27700c799bf3290b6869
Ricci.rudenko <richardrudenko555@gmail.com>
2023-12-26 13:29:35 +0100

    Change css file and add vans detail logic

--- index.jsx
+++ index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from './pages/Vans';
+import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
 
 import "./server"
@@ -15,14 +15,14 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
-        <Link to="/Vans">Vans</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/Vans" element={<Vans />} />
-        <Route path="/Vans/:id" element={<VanDetail/>} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,34 +1,33 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-
 export default function Vans() {
     const [vans, setVans] = React.useState([])
-
-    React.useState(() => {
+    
+    React.useEffect(() => {
         fetch("/api/vans")
             .then(res => res.json())
             .then(data => setVans(data.vans))
     }, [])
 
-    const vanElement = vans.map((van) => (
-        <section key={van.id} className="section-items">
+    const vanElements = vans.map(van => (
+        <div key={van.id} className="van-tile">
             <Link to={`/vans/${van.id}`}>
-                <img className="car-image" src={van.imageUrl} alt="Car" />
+                <img src={van.imageUrl} />
                 <div className="van-info">
                     <h3>{van.name}</h3>
                     <p>${van.price}<span>/day</span></p>
                 </div>
                 <i className={`van-type ${van.type} selected`}>{van.type}</i>
             </Link>
-        </section>
+        </div>
     ))
 
     return (
-        <div className="vans">
-            <h2>Explore our van options</h2>
-            <div className="container-items">
-                {vanElement}
+        <div className="van-list-container">
+            <h1>Explore our van options</h1>
+            <div className="van-list">
+                {vanElements}
             </div>
         </div>
     )
