3dcbdebc6f04d0b35175b80f203abdf45ec76b57
Ricci.rudenko <richardrudenko555@gmail.com>
2024-01-06 21:01:27 +0100

    Update login

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { useNavigate, useLocation } from "react-router-dom"
+import { loginUser } from "../api";
 
 export default function Login() {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    const [error, setError] = React.useState(null)
+    const [status, setStatus] = React.useState("idle")
+
     const location = useLocation()
+    const navigate = useNavigate()
+
     function handleSubmit(e) {
         e.preventDefault()
-        console.log(loginFormData)
+        setStatus("submitting")
+        loginUser(loginFormData)
+        .then(data => {
+            setError(null)
+            navigate("/host")
+        })
+        .catch(err =>{
+            setError(err)
+        })
+        .finally(() => {
+            setStatus("idle")
+        })
+
     }
 
     function handleChange(e) {
@@ -21,8 +39,9 @@ export default function Login() {
 
     return (
         <div className="login-container">
-            {location.state?.message && <h3 className="login-first">{location.state.message}</h3>}
+            {location.state?.message && <h3 className="login-error">{location.state.message}</h3>}
             <h1>Sign in to your account</h1>
+            {error?.message && <h1 className="login-error">{error.message}</h1>}
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
@@ -38,7 +57,10 @@ export default function Login() {
                     placeholder="Password"
                     value={loginFormData.password}
                 />
-                <button>Log in</button>
+                <button>{status === "submitting" 
+                ? "Log in..."
+                : "Log in"
+            }</button>
             </form>
         </div>
     )
