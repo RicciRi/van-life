f0601c4c25efb5941d2b6d7d712c4e45a814a976
Ricci.rudenko <richardrudenko555@gmail.com>
2024-01-21 09:28:02 +0100

    Update h1 home

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom"
 export default function Home() {
     return (
         <div className="home-container">
-            <h1>You got the travel plans, we got the travel vans.</h1>
-            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+            <h1>You’ve got plans?
+             We’ve got vans!</h1>
+            <p>Add adventure to your life by joining #vanlife. Rent the van that will make your trip unforgettable.</p>
             <Link to="vans">Find your van</Link>
         </div>
     )
